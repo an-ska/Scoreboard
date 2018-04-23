1f4a3c3951743bd57f1350c4fa2110a6dd4dad55
an-ska <1.anna.piotrowska@gmail.com>
2018-04-23 21:44:54 +0200

    Init commit.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, {Fragment} from 'react';
+import {hot} from 'react-hot-loader';
+import styles from './App.scss';
+
+const App = () =>
+(<Fragment>
+  <div className=''>
+    <div>test</div>
+  </div>
+</Fragment>);
+
+export default hot(module)(App);
