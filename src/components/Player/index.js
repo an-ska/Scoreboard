06f54455717bbff9377cb1368b145aa3f89d495d
an-ska <1.anna.piotrowska@gmail.com>
2018-05-05 19:55:26 +0200

    Font awesome icon for removing player added.

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -4,7 +4,7 @@ import PlayerPoints from '../PlayerPoints';
 
 const Player = ({playerId, playerName, removePlayer, playerPoints, decreasePointsByOne, increasePointsByOne}) => (
   <li id={playerId} className={styles.player}>
-    <span className={styles.removeIcon} onClick={() => removePlayer(playerId)}>x</span>
+    <i className={`${styles.removeIcon} fas fa-user-times`} onClick={() => removePlayer(playerId)}></i>
     <p className={styles.playerName}>{playerName}</p>
     <PlayerPoints
       playerId={playerId}
